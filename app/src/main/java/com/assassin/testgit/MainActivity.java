858f3194f6a3f7035479e76ac3f30e1c2748c597
shaycormac <574583006@qq.com>
2017-04-25 08:58:19 +0800

    第二次提交

--- app/src/main/java/com/assassin/testgit/MainActivity.java
+++ app/src/main/java/com/assassin/testgit/MainActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) 
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //做了一次简单的提交，测试一下
+        
     }
 }
